a01c783b440a82c0f77b85ea5ffab5279024a4af
Avadhutjc <Avadhutjc@gmail.com>
2021-12-19 23:22:12 +0530

    first commit

--- app/src/main/java/com/ajc/financialcalculator/MainActivity.kt
+++ app/src/main/java/com/ajc/financialcalculator/MainActivity.kt
@@ -9,3 +9,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+git add * -f
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/avadhutjc/Financial-Calculator.git
+git push -u origin main
